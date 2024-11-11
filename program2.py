e697bfdc6326a9867493b22843dc726ac2749aa4
Shivashankar9052 <kavalishiva33@gmail.com>
2024-11-11 13:46:57 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -3,11 +3,7 @@ def decode_message( s: str, p: str) -> bool:
 # write your code here
   m, n = len(s), len(p)
     dp = [[False] * (n + 1) for _ in range(m + 1)]
-    
-    
     dp[0][0] = True
-    
-   
     for j in range(1, n + 1):
         if p[j - 1] == '*':
             dp[0][j] = dp[0][j - 1]
@@ -19,7 +15,6 @@ def decode_message( s: str, p: str) -> bool:
                 dp[i][j] = dp[i - 1][j - 1]  # Exact match or '?' match
             elif p[j - 1] == '*':
                 dp[i][j] = dp[i - 1][j] or dp[i][j - 1]  # '*' can match empty or one/more chars
-    
     
     return dp[m][n]
   
