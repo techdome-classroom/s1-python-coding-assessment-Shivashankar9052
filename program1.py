e0e9e7672e6e5f29e918946304d4a03edc72fcac
Shivashankar9052 <kavalishiva33@gmail.com>
2024-11-11 13:14:12 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -2,5 +2,52 @@ class Solution:
    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
     #    write your code here
+       def dfs(i, j):
+            # If the current position is out of bounds or is water ('W'), return
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 'W':
+                return
+            
+            # Mark the current land as visited (by turning it into water 'W')
+            grid[i][j] = 'W'
+            
+            # Explore all 4 possible directions: up, down, left, right
+            dfs(i - 1, j)  # up
+            dfs(i + 1, j)  # down
+            dfs(i, j - 1)  # left
+            dfs(i, j + 1)  # right
+        
+        # Count the number of distinct islands
+        island_count = 0
+        
+        # Loop through the entire grid
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                # When an unvisited land is found, it's a new island
+                if grid[i][j] == 'L':
+                    island_count += 1
+                    # Start DFS to mark all connected land as visited
+                    dfs(i, j)
+        
+        return island_count
+
+       grid = [
+    ["L", "L", "L", "L", "W"],
+    ["L", "L", "W", "L", "W"],
+    ["L", "L", "W", "W", "W"],
+    ["W", "W", "W", "W", "W"]
+]
+solution = Solution()
+print(solution.getTotalIsles(grid))  # Output: 1
+
+grid = [
+    ["L", "L", "W", "W", "W"],
+    ["L", "L", "W", "W", "W"],
+    ["W", "W", "L", "W", "W"],
+    ["W", "W", "W", "L", "L"]
+]
+solution = Solution()
+print(solution.getTotalIsles(grid))  # Output: 3
+
+
                     
         return 0
