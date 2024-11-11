5a54526a61a4d743db39303190a25346701b931b
Shivashankar9052 <kavalishiva33@gmail.com>
2024-11-11 13:55:48 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,12 +1,13 @@
 def decode_message( s: str, p: str) -> bool:
 
 # write your code here
-  m, n = len(s), len(p)
+ m, n = len(s), len(p)
     dp = [[False] * (n + 1) for _ in range(m + 1)]
-    dp[0][0] = True
-    for j in range(1, n + 1):
+     dp[0][0] = True
+     for j in range(1, n + 1):
         if p[j - 1] == '*':
             dp[0][j] = dp[0][j - 1]
+    
     # Fill the dp table
     for i in range(1, m + 1):
         for j in range(1, n + 1):
@@ -15,5 +16,8 @@ def decode_message( s: str, p: str) -> bool:
             elif p[j - 1] == '*':
                 dp[i][j] = dp[i - 1][j] or dp[i][j - 1]  # '*' can match empty or one/more chars
     return dp[m][n]
+print(decode_message("aa", "*"))   
+print(decode_message("aa", "a"))   
+print(decode_message("cb", "?a"))   
   
        
